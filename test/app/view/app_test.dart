733e23452f0908ede0f9cbbc5f703aa71061586d
Talha Javed Khan <talhakhan1297@yahoo.com>
2023-10-01 18:27:26 +0500

    Updated test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,5 +1,11 @@
+import 'package:albums/app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('App', () {});
+  group('App', () {
+    testWidgets('renders App', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(App), findsOneWidget);
+    });
+  });
 }
